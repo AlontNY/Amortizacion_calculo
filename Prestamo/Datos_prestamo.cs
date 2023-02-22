480683be21bbe95eadca724d5c7f41d19c5eba77
AlontNY <89867912+AlontNY@users.noreply.github.com>
2023-02-22 11:43:00 -0600

    calculo de tabla de amortización

---
+++ Prestamo/Datos_prestamo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Prestamo
+{
+    class Datos_prestamo
+    {
+        float monto_capital;
+        float tasaInteresAnual;
+        int numCuotas;
+        public float Monto_capital
+        {
+            get { return monto_capital; }
+            set { monto_capital = value; }
+        }
+        public float TasaInteresAnual
+        {
+            get { return tasaInteresAnual;}
+            set { tasaInteresAnual = value;}
+        }
+        public int NumCuotas
+        {
+            get { return numCuotas; }
+            set { numCuotas = value;}
+        }
+
+        //////////////
+        /// no cambia una vez calculado
+        
+        public float ImporteCapital()
+        {
+            return  this.monto_capital / this.numCuotas;
+           
+        }
+       
+        public float MontoInteres(float prestamo) {
+            
+            return  prestamo *(this.tasaInteresAnual/100/360)*30 ;
+            
+        }
+
+
+
+    }
+}
---
+++ Prestamo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Prestamo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var Prestamo = new Datos_prestamo();
+            Prestamo.Monto_capital = 12000;
+            Prestamo.TasaInteresAnual = 18;
+            Prestamo.NumCuotas = 24;
+            var deuda = Prestamo.Monto_capital;
+            float pago = Prestamo.ImporteCapital();
+            
+
+            for (int i = 1; i <= Prestamo.NumCuotas; i++)
+            {
+
+                Console.WriteLine("NumCuota: "+ i);
+                Console.WriteLine("montoCapital: " + pago);
+                Console.WriteLine("montoInteres: " + Prestamo.MontoInteres(deuda));
+                deuda = deuda - pago;
+                Console.WriteLine("SaldoInsolutoCredito: " + deuda);
+                Console.WriteLine("\n\n");
+
+
+            }
+
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
+
+    
+    
